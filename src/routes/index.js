9e6c40efd48f21c337529ccb942956c1f52251b3
Jean Paul Saucedo <jpscorp21@gmail.com>
2019-01-27 09:38:51 -0300

    Proyecto de inicio

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', (req, res) => {
   res.send('Bienvenido a la aplicacion');
 });
 
+router.get('/example', (req, res) => {
+  res.send('Ejemplo');
+});
+
+router.get('/app', (req, res) => {
+  res.send('Ejemplo');
+});
+
+router.get('/otroejemplo', (req, res) => {
+  res.send('Otro ejemplo');
+});
+
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
-import bodyParser from 'body-parser';
 import express from 'express';
-import router from './routes';
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api/v1', router);
+app.set('port', process.env.PORT || 3000);
 
-app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+app.listen(app.get('port'), () => {
+  console.log(`Servidor escuchando en el puerto ${app.get('port')}`);
 });
 
 export default app;
